5aba8722ac4fd1f7b98776f053e00b540c2e4188
tongheshang <yunshixin8521894@sohu.com>
2018-01-06 21:21:23 +0800

    modify some files

---
+++ annotationPractice/src/test/java/Test.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tongheshang on 2018/1/6.
+ * https://twitter.com/heshengbang
+ * https://github.com/tongheshang
+ * email: trulyheshengbang@gmail.com
+ */
+public class Test {
+    @org.junit.Test
+    public void test1() {
+        List<Integer> list = new ArrayList<Integer>();
+        int num = 1;
+        while (num <= 100) {
+            list.add(num);
+            num++;
+        }
+        num = 1;
+        while (list.size() > 1) {
+            if (num % 3 == 0) {
+                list.remove(0);
+            } else {
+                int temp = list.get(0);
+                list.remove(0);
+                list.add(list.size(), temp);
+            }
+            num++;
+        }
+        System.out.print(list.get(0));
+    }
+}
