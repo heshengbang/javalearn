87790ff5c9abc2a5b634c2e24dc9898ba650ab06
tongheshang <yunshixin8521894@sohu.com>
2018-01-18 19:09:41 +0800

    update file

--- threadPractice/src/test/java/com/hsb/java/thread/MyThread.java
+++ threadPractice/src/test/java/com/hsb/java/thread/MyThread.java
@@ -15,13 +15,33 @@ package com.hsb.java.thread;
  */
 
 public class MyThread extends Thread {
+
+    public MyThread(String name, int pro) {
+        super(name);// 设置线程的名称
+        this.setPriority(pro);// 设置优先级
+    }
+
     @Override
     public void run() {
-        System.out.println("fuck you");
+        for (int i = 0; i < 30; i++) {
+            System.out.println(this.getName() + "线程第" + i + "次执行！");
+            if (i % 5 == 0)
+                Thread.yield();
+        }
     }
 
     public static void main(String[] args) {
-        new MyThread().start();
-        new MyThread().run();
+//        new MyThread("低级", Thread.MAX_PRIORITY).start();
+//        new MyThread("中级", Thread.NORM_PRIORITY).start();
+//        new MyThread("高级", Thread.MIN_PRIORITY).start();
+
+        String s = new String("1");
+        s = s.intern();
+        String s2 = "1";
+        System.out.println(s == s2);
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
     }
 }
