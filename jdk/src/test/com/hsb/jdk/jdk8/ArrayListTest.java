1084f05d842f1655485f63a21845fe734f24b847
heshengbang <yunshixin8521894@sohu.com>
2018-02-08 18:16:13 +0800

    delete some things

---
+++ jdk/src/test/com/hsb/jdk/jdk8/ArrayListTest.java
@@ -0,0 +1,30 @@
+package com.hsb.jdk.jdk8;
+        /*
+         * Copyright ©2011-2017 hsb
+         */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("FUck");
+        list.add("shit");
+        list.add("bastard");
+
+//        for (String string: list) {
+//            System.out.println(string);
+//            list.remove(string);
+//            list.add("FUCK again");
+//        }
+
+//        for (int i = 0; i < list.size(); i++) {
+//            list.add("FUCK");
+//        }
+
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
+}
--- threadPractice/src/test/java/com/hsb/java/thread/MyThread.java
+++ threadPractice/src/test/java/com/hsb/java/thread/MyThread.java
@@ -3,17 +3,6 @@ package com.hsb.java.thread;
          * Copyright ©2011-2017 hsb
          */
 
-/**
- * 功能描述:&lt;/br&gt;
- * Copyright ©2011-2016 中国民航信息网络股份有限公司 &lt;/br&gt;
- *
- * @author hhu@travelsky.com 何虎
- * @version 1.0
- * @time 2018/1/16 14:06
- * ************************************************
- * @since
- */
-
 public class MyThread extends Thread {
 
     public MyThread(String name, int pro) {
