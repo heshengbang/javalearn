79c85e328315175099b99e84dbd433c2533506d3
heshengbang <yunshixin8521894@sohu.com>
2018-03-12 16:40:22 +0800

    add whole merge sort

--- base/src/test/java/com/hsb/algorithm/MergeSort.java
+++ base/src/test/java/com/hsb/algorithm/MergeSort.java
@@ -43,77 +43,61 @@ public class MergeSort {
 //    }
     //　private　static　long　sum　=　0;
 
+
     /**
-     * 　*　<pre>
-     * 　*　二路归并
-     * 　*　原理：将两个有序表合并和一个有序表
-     * 　*　</pre>
-     * 　*
-     * 　*　@param　a
-     * 　*　@param　s
-     * 　*　第一个有序表的起始下标
-     * 　*　@param　m
-     * 　*　第二个有序表的起始下标
-     * 　*　@param　t
-     * 　*　第二个有序表的结束小标
-     * 　*
+     * 先拆分数组并排序，然后再合并
+     * @param a 待分组排序的大数组
+     * @param low 小数列的起始位置
+     * @param high 小数列的结束位置（包含在数组内）
      */
-    private static void merge(int[] a, int s, int m, int t) {
-        int[] tmp = new int[t - s + 1];
-        int i = s, j = m, k = 0;
-        while (i < m && j <= t) {
-            if (a[i] <= a[j]) {
-                tmp[k] = a[i];
-                k++;
-                i++;
-            } else {
-                tmp[k] = a[j];
-                j++;
-                k++;
-            }
+    private static void mergeSort(int[] a, int low, int high) {
+        //将数列分为两部分
+        int mid = (low + high) / 2;
+        if (low < high) {
+            //左边部分进行拆分并排序
+            mergeSort(a, low, mid);
+            //右边部分进行拆分并排序
+            mergeSort(a, mid + 1, high);
+            //将已经有序的两部分进行合并
+            merge(a, low, mid, high);
         }
-        while (i < m) {
-            tmp[k] = a[i];
-            i++;
-            k++;
-        }
-        while (j <= t) {
-            tmp[k] = a[j];
-            j++;
-            k++;
-        }
-        System.arraycopy(tmp, 0, a, s, tmp.length);
     }
 
     /**
-     * 　*
-     * 　*　@param　a
-     * 　*　@param　s
-     * 　*　@param　len
-     * 　*　每次归并的有序集合的长度
+     * 合并有序数组
+     * @param a 在各个小区域有序的数组
+     * @param low 小数列在大数组中的开始位置
+     * @param mid 两个有序小数列的中点位置
+     * @param high 小数列在大数组中的结束位置
      */
-    private static void mergeSort(int[] a, int s, int len) {
-        int size = a.length;
-        int mid = size / (len << 1);
-        int c = size & ((len << 1) - 1);
-        //　-------归并到只剩一个有序集合的时候结束算法-------//
-        if (mid == 0)
-            return;
-        //　------进行一趟归并排序-------//
-        for (int i = 0; i < mid; ++i) {
-            s = i * 2 * len;
-            merge(a, s, s + len, (len << 1) + s - 1);
+    private static void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
         }
-        //　-------将剩下的数和倒数一个有序集合归并-------//
-        if (c != 0)
-            merge(a, size - c - 2 * len, size - c, size - 1);
-        //　-------递归执行下一趟归并排序------//
-        mergeSort(a, 0, 2 * len);
+
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+
+        System.arraycopy(temp, 0, a, low, temp.length);
     }
 
     public static void main(String[] args) {
-        int[] a = RandomGenerator.randomArray(100, 1000);
-        mergeSort(a, 0, 1);
+        int[] a = RandomGenerator.randomArray(100, 10000);
+        mergeSort(a, 0, a.length-1);
         for (int anA : a) {
             System.out.print(anA + "　");
         }
